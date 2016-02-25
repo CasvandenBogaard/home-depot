04a1c29afba513a84907c544269965c99cfe5d62
Luuk Scholten <info@luukscholten.com>
2016-02-25 17:00:20 +0100

    Overlap regression

--- rfr_different_params.py
+++ rfr_different_params.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -12,23 +13,32 @@ N = df_train.shape[0]
 
 df_all = pd.concat((df_train, df_test), axis=0, ignore_index=True)
 df_all = pd.merge(df_all, df_description, how='left', on='product_uid')
-df_all['product_info'] = df_all['product_title'] + "\t" + df_all['product_description'] + "\t" + df_all['search_term']
+df_all['descr_search'] = df_all['product_description'] + "\t" + df_all['search_term']
+df_all['descr_overlap'] = df_all['descr_search'].map(lambda x: " ".join(set(x.split("\t")[0].split(" ")).intersection(set(x.split("\t")[1].split(" ")))))
+df_all['title_search'] = df_all['product_title'] + "\t" + df_all['search_term']
+df_all['title_overlap'] = df_all['title_search'].map(lambda x: " ".join(set(x.split("\t")[0].split(" ")).intersection(set(x.split("\t")[1].split(" ")))))
 
+tfv_descr = TfidfVectorizer(analyzer='word')
+tfv_descr.fit(df_all['descr_search'])
+td_descr = tfv_descr.transform(df_all['descr_overlap'])
+tfv_title = TfidfVectorizer(analyzer='word')
+tfv_title.fit(df_all['title_search'])
+td_title = tfv_title.transform(df_all['title_overlap'])
 
-tfv = TfidfVectorizer(ngram_range=(1,2), analyzer='word', stop_words='english')
-td = tfv.fit_transform(df_all['product_info'])
+tsvd_descr = TruncatedSVD(random_state=30)
+result_descr = tsvd_descr.fit_transform(td_descr)
+tsvd_title = TruncatedSVD(random_state=30)
+result_title = tsvd_title.fit_transform(td_title)
 
-tsvd = TruncatedSVD(n_components=50)
-result = tsvd.fit_transform(td)
-
-x_train = result[:N]
-x_test  = result[N:]
+x_train = np.concatenate((result_descr[:N], result_title[:N]), axis=1)
+x_test  = np.concatenate((result_descr[N:], result_title[N:]), axis=1)
 y = df_all.iloc[:N]['relevance'].values
+y_test = df_all.iloc[N:]['relevance'].values
 id_test = df_all.iloc[N:]['id']
 
-clf = RandomForestRegressor(n_estimators=50)
+clf = RandomForestRegressor(n_estimators=20, random_state=30, verbose=True)
 clf.fit(x_train, y)
 
 y_pred = clf.predict(x_test)
 
-pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv('results/submission.csv',index=False)
+pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv('results/submission.csv', index=False)
