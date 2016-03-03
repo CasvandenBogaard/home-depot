13d42d8e4ff657d6156d745e7546fc940b1ae0b1
Luuk Scholten <info@luukscholten.com>
2016-03-03 10:16:31 +0100

    Brands and colors in k-fold stuff

--- rfr_different_params.py
+++ rfr_different_params.py
@@ -1,40 +1,35 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import TruncatedSVD
 from sklearn import cross_validation
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
 def rmse(true, test):
     return mean_squared_error(true, test)**0.5
 
-def tfidf(data):
-    data['descr_search'] = data['product_description'] + "\t" + data['search_term']
-    data['descr_overlap'] = data['descr_search'].map(lambda x: " ".join(set(x.split("\t")[0].split(" ")).intersection(set(x.split("\t")[1].split(" ")))))
-    data['title_search'] = data['product_title'] + "\t" + data['search_term']
-    data['title_overlap'] = data['title_search'].map(lambda x: " ".join(set(x.split("\t")[0].split(" ")).intersection(set(x.split("\t")[1].split(" ")))))
+def features(data):
+    data['descr_overlap'] = [" ".join(set(x.split(" ")).intersection(set(y.split(" ")))) for x,y in zip(data['search_term'], data['product_description'])]
+    data['title_overlap'] = [" ".join(set(x.split(" ")).intersection(set(y.split(" ")))) for x,y in zip(data['search_term'], data['product_title'])]
+    data['descr_match'] = [1 if x in y else 0 for x,y in zip(data['search_term'], data['product_description'])]
+    data['title_match'] = [1 if x in y else 0 for x,y in zip(data['search_term'], data['product_title'])]
+    data['brand_match'] = [1 if str(y) in x else 0 for x,y in zip(data['search_term'], data['brand'])]
+    data['color_match'] = [1 if str(y) in x else 0 for x,y in zip(data['search_term'], data['color'])]
 
-    print("Learning tf idf vectors")
-    tfv1 = TfidfVectorizer(analyzer='word')
-    tfv1.fit(data['descr_search'])
-    td1 = tfv1.transform(data['descr_overlap'])
-    tfv2 = TfidfVectorizer(analyzer='word')
-    tfv2.fit(data['title_search'])
-    td2 = tfv2.transform(data['title_overlap'])
+    df = pd.DataFrame({
+        'descr_overlap': data['descr_overlap'].map(lambda x: len(x)),
+        'title_overlap': data['title_overlap'].map(lambda x: len(x)),
+        'descr_match': data['descr_match'],
+        'title_match': data['title_match'],
+        'brand_match': data['brand_match'],
+        'color_match': data['color_match'],
+    })
 
-    print("Reducing feature space")
-    tsvd1 = TruncatedSVD(random_state=30)
-    result1 = tsvd1.fit_transform(td1)
-    tsvd2 = TruncatedSVD(random_state=30)
-    result2 = tsvd2.fit_transform(td2)
-    return np.concatenate((result1,result2), axis=1)
+    return df.iloc[:]
 
 def extract_features(train, test):
-    df_all = pd.concat((train, test), axis=0, ignore_index=True)
-
-    result_train = tfidf(train)
-    result_test  = tfidf(test)
+    result_train = features(train)
+    result_test  = features(test)
 
     x_train = result_train
     x_test  = result_test
@@ -50,21 +45,31 @@ df_attributes = pd.read_csv('data/attributes.csv', encoding="ISO-8859-1")
 
 df_train = pd.merge(df_train, df_description, how='left', on='product_uid')
 
+brands = df_attributes[df_attributes.name=='MFG Brand Name']
+df_train = pd.merge(df_train, brands, how='left', on='product_uid')
+df_train.drop('name',inplace=True,axis=1)
+df_train.columns = df_train.columns.str.replace('value','brand')
+
+colors = df_attributes[df_attributes.name=='Color Family']
+df_train = pd.merge(df_train, colors, how='left', on='product_uid')
+df_train.drop('name',inplace=True,axis=1)
+df_train.columns = df_train.columns.str.replace('value','color')
+
 N = df_train.shape[0]
 
+# Cross-validation setup
 kf = cross_validation.KFold(N, n_folds=10, random_state=30)
 for train, test in kf:
     train_set = df_train.loc[train]
     test_set  = df_train.loc[test]
     x_train, x_test, y_train, y_test = extract_features(train_set, test_set)
 
-    clf = RandomForestRegressor(n_estimators=20, random_state=30, verbose=True)
+    clf = RandomForestRegressor(verbose=True)
+    print("Fitting random forest regressor")
     clf.fit(x_train, y_train)
     y_pred = clf.predict(x_test)
+
     print(rmse(y_test, y_pred))
-
-
-exit(1)
 
 # This is for handing in results
 df_test = pd.read_csv('data/test.csv', encoding="ISO-8859-1")
@@ -73,7 +78,7 @@ df_test = pd.merge(df_test, df_description, how='left', on='product_uid')
 id_test = df_test['id']
 
 x_train, x_test, y_train = extract_features(df_train, df_test)
-clf = RandomForestRegressor(n_estimators=20, random_state=30, verbose=True)
+clf = RandomForestRegressor(n_estimators=30, random_state=30, verbose=True)
 clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
 
