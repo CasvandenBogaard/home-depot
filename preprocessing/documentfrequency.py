ac96d84d42dc23ac518eb6ae95a195adacbbdf64
Luuk Scholten <info@luukscholten.com>
2016-04-14 18:15:15 +0200

    Small optimization for document count generation

--- preprocessing/documentfrequency.py
+++ preprocessing/documentfrequency.py
@@ -6,11 +6,21 @@ import pickle
 
 os.chdir('../')
 
-vect = CountVectorizer(input='filename', token_pattern='\\b[^\s]+\\b')
-result = vect.fit_transform(['data/word2vec/sentences.txt'])
 
-with open('data/termcounts/vocab.pkl', 'wb') as file:
-    pickle.dump(vect.vocabulary_, file)
 
-with open('data/termcounts/counts.pkl', 'wb') as file:
-    pickle.dump(result, file)
+DO_THIS = ["tf", "df"]
+
+if "tf" in DO_THIS:
+    vect = CountVectorizer(input='filename', token_pattern='[^\\s]+')
+    result = vect.fit_transform(['data/word2vec/sentences.txt'])
+
+    dict = {}
+
+    with open('data/termcounts/vocab.pkl', 'wb') as file:
+        pickle.dump(vect.vocabulary_, file)
+
+    with open('data/termcounts/counts.pkl', 'wb') as file:
+        pickle.dump(result, file)
+
+if "df" in DO_THIS:
+    vect = CountVectorizer(token_pattern='[^\\s]+')
